65e0f0eddb7518e1cdb5867fd653d691295d06a2
Sofia Levin <sofiafontes@me.com>
2020-04-20 15:46:36 -0700

    Added constructor to cpu.py.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,10 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.memory = [0] * 256
+        self.register = [0] * 8
+        self.PC = [0] * 8
+
 
     def load(self):
         """Load a program into memory."""
